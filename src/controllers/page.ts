61d3a316fd3bcfc616286ee9cbc9ed7b2ec0e322
BrunoSantosPK <bruno.ajsch@yahoo.com>
2022-07-26 21:03:59 -0300

    esqueleto da pagina de formulario

--- src/controllers/page.ts
+++ src/controllers/page.ts
@@ -4,16 +4,16 @@ export default class PageController {
 
     static async formGamerTypes(request: Request, response: Response) {
         const words = [
-            [{word: "Comunicativa", id: 1}, {word: "Empática", id: 1}],
-            [{word: "Confiante", id: 1}, {word: "Criativa", id: 1}],
-            [{word: "Comunicativa", id: 1}, {word: "Analítica", id: 1}],
-            [{word: "Empática", id: 1}, {word: "Criativa", id: 1}],
-            [{word: "Comunicativa", id: 1}, {word: "Confiante", id: 1}],
-            [{word: "Analítica", id: 1}, {word: "Confiante", id: 1}],
-            [{word: "Comunicativa", id: 1}, {word: "Criativa", id: 1}],
-            [{word: "Empática", id: 1}, {word: "Analítica", id: 1}],
-            [{word: "Analítica", id: 1}, {word: "Criativa", id: 1}],
-            [{word: "Empática", id: 1}, {word: "Confiante", id: 1}]
+            [{word: "Comunicativa", id: 1}, {word: "Empática", id: 2}],
+            [{word: "Confiante", id: 3}, {word: "Criativa", id: 4}],
+            [{word: "Comunicativa", id: 1}, {word: "Analítica", id: 5}],
+            [{word: "Empática", id: 2}, {word: "Criativa", id: 4}],
+            [{word: "Comunicativa", id: 1}, {word: "Confiante", id: 3}],
+            [{word: "Analítica", id: 5}, {word: "Confiante", id: 3}],
+            [{word: "Comunicativa", id: 1}, {word: "Criativa", id: 4}],
+            [{word: "Empática", id: 2}, {word: "Analítica", id: 5}],
+            [{word: "Analítica", id: 5}, {word: "Criativa", id: 4}],
+            [{word: "Empática", id: 2}, {word: "Confiante", id: 3}]
         ];
         response.render("pages/form-gamer", {combination: words});
     }
